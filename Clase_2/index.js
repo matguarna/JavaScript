6125d10921e3f817bf2d9373fe7b9171ab064b2d
Matias Guarna <matias.guarna@gmail.com>
2021-12-20 19:33:43 -0300

    Sub def 2

--- Clase_2/index.js
+++ Clase_2/index.js
@@ -50,3 +50,77 @@ if (esMayor5) {
 } else {
   console.log("Es boolean false");
 }
+
+//Operadores logicos
+
+let a = 10;
+let b = 20; 
+
+if (a == b){
+    console.log("A es igual a B");
+}
+
+if (a === b) {
+    console.log("A y B son totalmente iguales");
+}
+
+if (a != b) {
+    console.log("A es distinto a B");
+}
+
+if (a !== b) {
+    console.log("A es totalmente distinto a B");
+}
+
+if (a < b ) {
+    console.log("A es menor que B");
+} else {
+    console.log("A es menor que B");
+}
+
+let tempA = true;
+let tempB = false;
+let tempC = true;
+
+if (tempA && tempB){
+    console.log("Ambos son verdaderos");
+}
+
+if (tempA || tempB){
+    console.log("Ambos o uno de los dos es verdadero");
+}
+
+
+//usamos ! para cambiar el valor de verdadero a falso o viceversa. 
+
+if (!tempA || !!tempC){ 
+    console.log("Ambos o uno de los dos es verdadero");
+}
+
+if (tempA && (!tempB || tempC)) { 
+    console.log("Es true");
+}
+
+if (!!!!!!tempA){
+    console.log("tempA es true");
+}
+
+//Condiciones compuestas con ||
+let nombreIngresado = prompt("Ingresar nombre");
+let apellidoIngresado = prompt("Ingresar apellido");
+
+if ((nombreIngresado == "ANA") || (nombreIngresado == "ana")) {
+    console.log("El nombre ingresado es Ana");
+} else {
+    console.log("El nombre ingresado no es Ana");
+}
+
+//Condiciones compuestas con || y &&
+let nombreIngresado2 = prompt("Ingrese nombre");
+
+if ((nombreIngresado2 !="") && ((nombreIngresado2 == "EMA") || (nombreIngresado2 == "ema"))){
+    alert("Hola Ema")
+}else {
+    alert("Error: Ingresa un nombre valido");
+}
+
