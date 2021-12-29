f12f606a0bdbab5a389b559db1130d65e3c43a3c
Matias Guarna <matias.guarna@gmail.com>
2021-12-28 21:25:53 -0300

    Def 4 s

--- Clase_4/desafio4.js
+++ Clase_4/desafio4.js
@@ -5,55 +5,24 @@ function mostrar(valor) {
 }
 mostrar(resultado);
 
-
+///////
 
 const asignarTurno = function (fecha, hora, nombre, edad, tratamiento) {
+  console.log(`La fecha ingresada es: ${fecha}`);
+  console.log(`La hora ingresada es: ${hora}`);
+  console.log(`El nombre ingresado es: ${nombre}`);
+  console.log(`La edad ingresada es: ${edad}`);
+  console.log(`El tratamiento ingresado es: ${tratamiento}`);
+};
+
+let turno = prompt("Si quiere reservar un turno ingrese SI \nDe lo contrario, ingrese SALIR");
+
+while (turno != "SALIR") {
   let fechaT = prompt("Ingrese la fecha deseada");
   let horaT = prompt("Ingrese la hora deseada");
   let nombreT = prompt("Ingrese su nombre y apellido");
   let edadT = prompt("Ingrese su edad");
   let tratamientoT = prompt("Si lo sabe, ingrese el tipo de tratamiento que desea solicitar");
-
-  console.log(`La fecha ingresada es: ${fechaT}`);
-  console.log(`La hora ingresada es: ${horaT}`);
-  console.log(`El nombre ingresado es: ${nombreT}`);
-  console.log(`La edad ingresada es: ${edadT}`);
-  console.log(`El tratamiento ingresado es: ${tratamientoT}`);
-};
-
-let turno = prompt("Si quiere reservar un turno ingrese SI \nDe lo contrario, ingrese SALIR");
-while (turno != "SALIR") {
-  asignarTurno();
-
-  turno = prompt(
-    `La fecha ingresada es: ${fechaT} \nLa hora ingresada es: ${horaT} \nEl nombre ingresado es: ${nombreT} \nLa edad ingresada es: ${edadT} \nEl tratamiento ingresado es: ${tratamientoT} \nSi los datos son correctos, escriba SALIR`
-  );
-}
-
-///////
-
-const asignarTurno = function (fecha, hora, nombre, edad, tratamiento) {
-  console.log(`La fecha ingresada es: ${fechaT}`);
-  console.log(`La hora ingresada es: ${horaT}`);
-  console.log(`El nombre ingresado es: ${nombreT}`);
-  console.log(`La edad ingresada es: ${edadT}`);
-  console.log(`El tratamiento ingresado es: ${tratamientoT}`);
-};
-
-let turno = prompt("Si quiere reservar un turno ingrese SI \nDe lo contrario, ingrese SALIR");
-
-let fechaT = 0;
-let horaT = 0;
-let nombreT = 0;
-let edadT = 0;
-let tratamientoT = 0;
-
-while (turno != "SALIR") {
-  fechaT = prompt("Ingrese la fecha deseada");
-  horaT = prompt("Ingrese la hora deseada");
-  nombreT = prompt("Ingrese su nombre y apellido");
-  edadT = prompt("Ingrese su edad");
-  tratamientoT = prompt("Si lo sabe, ingrese el tipo de tratamiento que desea solicitar");
 
   asignarTurno(fechaT, horaT, nombreT, edadT, tratamientoT);
 
