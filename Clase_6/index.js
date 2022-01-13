acad3b1f1a8ed868f06d29eae7b309b2c7819d5d
Matias Guarna <matias.guarna@gmail.com>
2022-01-12 23:44:42 -0300

    6 clas2

--- Clase_6/index.js
+++ Clase_6/index.js
@@ -3,31 +3,30 @@ const arrayB = [1, 2, "asd", false, 25];
 console.log(arrayB.length);
 console.log(arrayB[0]);
 
-
 //.lenght = la longitud de "lista", osea 6.
 const lista = ["a", "b", "c", "d", "e", "f", "g"];
-for (let i = 0; i<lista.length; i++){ 
-    console.log(lista[i]);
-    console.log(`soy una bolubi ${i}`);
+for (let i = 0; i < lista.length; i++) {
+  console.log(lista[i]);
+  console.log(`soy una bolubi ${i}`);
 }
 
 let index = 0;
-while (index<lista.length){
-    console.log(lista[index]);
-    index++
+while (index < lista.length) {
+  console.log(lista[index]);
+  index++;
 }
 
 //.toString()
-const arrayC = [1, 2, "asd", {nombre: "false"}, 25];
+const arrayC = [1, 2, "asd", { nombre: "false" }, 25];
 console.log(arrayC.toString());
 
 //.push()
-const arrayD = [1, 2, "asd", {nombre: "false"}, 25];
+const arrayD = [1, 2, "asd", { nombre: "false" }, 25];
 arrayD.push("ultimo");
 console.log(arrayD);
 
 //.join
-const arrayE = [1, 2, "asd", {nombre: "false"}, 25];
+const arrayE = [1, 2, "asd", { nombre: "false" }, 25];
 console.log(arrayE.join("@"));
 
 //.concat
@@ -37,7 +36,6 @@ console.log(arrayB.concat(arrayC));
 const arrayF = [0, 1, 2, 3, 4, 5];
 const arrayG = arrayF.slice(1, 3);
 console.log(arrayG);
-
 
 //Ejemplo aplicado: cargar array con entradas
 // const listaNombres = [];
@@ -53,6 +51,11 @@ console.log(arrayG);
 // alert(nuevaLista.join("\n"));
 //
 //Acceder al ultimo valor de un array
-console.log(arrayD[arrayD.length-1]);
+console.log(arrayD[arrayD.length - 1]);
 
 //Array de objetos
+const persona = { nombre: "Matias", apellido: "Guarna" };
+const persona1 = { nombre: "Majo", apellido: "Pacheco" };
+const list = [persona];
+list.push(persona1);
+console.log(list);
