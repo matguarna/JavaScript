e04004bc7c93515342bc36fffef98ae07fe42856
Matias Guarna <matias.guarna@gmail.com>
2022-01-18 21:40:44 -0300

    C. Eventos

---
+++ Clase_9/eventos.js
@@ -0,0 +1,14 @@
+const showAlert = () => {
+  alert("Holaaaa");
+};
+
+let button = document.getElementById("miBtn").addEventListener("click", showAlert);
+
+// let button = document.getElementById("miBtn").addEventListener("click", function () {
+//   alert("Funcion anonima");
+// });
+
+// let button = document.getElementById("miBtn").addEventListener("click", () => {
+//   alert("Funcion anonima");
+// });
+
