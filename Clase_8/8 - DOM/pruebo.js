d5cf0d4bcede5e5afe80b9c7322206e3238dd86e
Matias Guarna <matias.guarna@gmail.com>
2022-01-27 19:15:28 -0300

    al fin 8

--- Clase_8/8 - DOM/pruebo.js
+++ Clase_8/8 - DOM/pruebo.js
@@ -4,8 +4,8 @@ class Turnos {
     this.hora = hora;
     this.profesionalTurno = profesionalTurno;
     this.listaTurnos = listaTurnos;
-
   }
+
   // addTurno(turno) {
   //   this.listaTurnos.push(turno);
   // }
@@ -21,11 +21,24 @@ const turno6 = new Turnos("10/02/2022", "18:00", "");
 const listaTurnos = [];
 listaTurnos.push(turno1, turno2, turno3, turno4, turno5, turno6);
 
+const solicitarTurno = () => {
+  let solicTurno = document.getElementById("turnosForm");
+  let div = document.createElement("div");
+  div.innerHTML = `<input type="text" placeholder="Nombre" />
+  <input type="number" placeholder="Telefono" /> <input type="number" placeholder="Edad" /> <input type="date" placeholder="Fecha" /> <input type="text" placeholder="Horario" /> <input type="submit" value="Reservar" />`;
+  solicTurno.appendChild(div);
+  document.getElementById("stopButton1").disabled = true;
+};
+
 const mostrarTurnosDisponibles = () => {
-  let turnis = document.getElementById("turnosForm");
+  let turnosFunction = document.getElementById("turnosForm");
+  let p = document.createElement("p");
+  p.innerHTML = `Los días y horarios disponibles son: `;
+  turnosFunction.appendChild(p);
   for (const turno of listaTurnos) {
     let li = document.createElement("li");
-    li.innerHTML = turno;
-    turnis.appendChild(li);
+    li.innerHTML = `Día ${turno.fecha} a las ${turno.hora} hs. - ${turno.profesionalTurno}`;
+    turnosFunction.appendChild(li);
   }
+  document.getElementById("stopButton2").disabled = true;
 };
